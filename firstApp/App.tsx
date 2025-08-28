2e2fa9a1cfad75663094957241cd7bcb526db950
위건 <juhyeon2398@naver.com>
2025-08-28 22:48:42 +0900

    native 프로젝트 생성

---
+++ firstApp/App.tsx
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { StatusBar, useColorScheme } from 'react-native';
+import {
+  SafeAreaProvider,
+} from 'react-native-safe-area-context';
+import { Text } from 'react-native';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text>Hello, World!</Text>
+    </SafeAreaProvider>
+  );
+}
+
+
+
+export default App;
